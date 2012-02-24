2447b281419437d4a596a523c22d5fa1a5e4e00f
railnerd <railnerd@me.com>
2012-02-23 20:01:25 -0800

    fix getInitialState

--- jmri.js
+++ jmri.js
@@ -46,6 +46,7 @@ exports.xmlioRequest = function xmlioRequest(host,port,xml,callback) {
 	  });
   
 	  res.on('end',function () {
+	  	console.log('JMRI RESPONSE: '+ responseXML);
 		if (typeof(callback) == 'function') {
 			callback(responseXML.toString());
 		}
@@ -56,7 +57,15 @@ exports.xmlioRequest = function xmlioRequest(host,port,xml,callback) {
 	  console.log('problem with request: ' + e.message);
 	});
 	
-	console.log('sending: '+ postData);
+	console.log("JMRI REQUEST: "+postData);
 	req.write(postData);
 	req.end();
+}
+
+
+exports.getInitialState = function getInitialState(host,port,callback) {
+
+	var xml = "<xmlio><list><type>turnout</type></list><list><type>sensor</type></list></xmlio>"
+
+	xmlioRequest(host,port,xml,callback);
 }
