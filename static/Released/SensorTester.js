95734ef3bbe342acd585e99e633dc9b2893cadb6
svlPanelMaker <cerickson@mac.com>
2012-10-26 22:50:12 -0700

    Support for Block Detection
    -Adds support for block detection
    -Fixed a few authorization text SVG bugs
    -Display authorization text on both authorized and occupied blocks
    -Added SensorTester to emulate actual layout hardware detectors

---
+++ static/Released/SensorTester.js
@@ -0,0 +1,31 @@
+function panelInit(evt)
+{
+    for(var i = 1; i <= 8; i++)
+    {
+        var textElemID = JMRI_SENSOR_OBJID_PREFIX + i;
+        var textElem = svgDocument.getElementById(textElemID);
+        
+        setSVGText(textElemID, JMRI_SENSOR_INACTIVE);
+        setStyleSubAttribute(textElem, "cursor", "pointer");
+    }
+}
+
+function toggleState(whichElem)
+{
+    var textElem = whichElem.parentNode;
+
+    var newState;
+
+    if(getSVGText(textElem.id) == JMRI_SENSOR_INACTIVE)
+        newState = JMRI_SENSOR_ACTIVE;
+    else
+        newState = JMRI_SENSOR_INACTIVE;
+    
+    setSVGText(textElem.id, newState);
+    
+    var dataToSend = new Array();
+    
+    dataToSend.push(new ServerObject(textElem.id, SERVER_TYPE_DISPATCH, newState));
+    
+    serverSet(dataToSend);
+}
