953618317f1918872a27ea9167a52f18f579f2f9
svlPanelMaker <cerickson@mac>
2013-08-16 22:56:30 -0700

    Dayton Path Fixes
    Dayton Path Fixes

--- static/Released/Dayton.js
+++ static/Released/Dayton.js
@@ -34,16 +34,16 @@ function panelInit(evt)
 
 function DPL3Path()
 {
-	addTurnoutStateChangeRequest("TO174", 'N');
-	addTurnoutStateChangeRequest("TO172", 'R');
+	addTurnoutStateChangeRequest("TO174", 'R');
+	addTurnoutStateChangeRequest("TO172", 'N');
 
 	executePanelStateChangeRequests();
 }
 
 function DPL2Path()
 {
-	addTurnoutStateChangeRequest("TO174", 'R');
-	addTurnoutStateChangeRequest("TO172", 'R');
+	addTurnoutStateChangeRequest("TO174", 'N');
+	addTurnoutStateChangeRequest("TO172", 'N');
 
 	executePanelStateChangeRequests();
 }
@@ -51,7 +51,7 @@ function DPL2Path()
 function DPL1Path()
 {
 	addTurnoutStateChangeRequest("TO173", 'N');
-	addTurnoutStateChangeRequest("TO172", 'N');
+	addTurnoutStateChangeRequest("TO172", 'R');
 
 	executePanelStateChangeRequests();
 }
@@ -59,15 +59,37 @@ function DPL1Path()
 function PeacheyBrothersPath()
 {
 	addTurnoutStateChangeRequest("TO173", 'R');
-	addTurnoutStateChangeRequest("TO172", 'N');
+	addTurnoutStateChangeRequest("TO172", 'R');
 
 	executePanelStateChangeRequests();
 }
 
-function WiebeWafersPath()
+function DaytonIndustrialLeadPath()
 {
-	addTurnoutStateChangeRequest("TO167", 'R');
-	//addTurnoutStateChangeRequest("TO169", 'N');
+	addTurnoutStateChangeRequest("TO170", 'R');
+	addTurnoutStateChangeRequest("TO171", 'R');
+
+	executePanelStateChangeRequests();
+}
+
+function IndRunaroundNorthPath()
+{
+	addTurnoutStateChangeRequest("TO170", 'N');
+	addTurnoutStateChangeRequest("TO171", 'R');
+
+	executePanelStateChangeRequests();
+}
+
+function IndRunaroundSouthPath()
+{
+	addTurnoutStateChangeRequest("TO169", 'N');
+
+	executePanelStateChangeRequests();
+}
+
+function OnthaCRLLeadPath()
+{
+	addTurnoutStateChangeRequest("TO171", 'N');
 
 	executePanelStateChangeRequests();
 }
@@ -79,34 +101,56 @@ function OnthaSkidsPath()
 	executePanelStateChangeRequests();
 }
 
+function WiebeOnthaCRLPath()
+{
+    addTurnoutStateChangeRequest("TO167", 'N');
+	addTurnoutStateChangeRequest("TO168", 'N');
+
+	executePanelStateChangeRequests();
+}
+
+function WiebeWafersPath()
+{
+	addTurnoutStateChangeRequest("TO167", 'R');
+
+	executePanelStateChangeRequests();
+}
+
+function WiebeCRLLeadPath()
+{
+	addTurnoutStateChangeRequest("TO169", 'R');
+
+	executePanelStateChangeRequests();
+}
+
 function WarehouseNo9Path()
 {
-	addTurnoutStateChangeRequest("TO165", 'R');
-	addTurnoutStateChangeRequest("TO166", 'N');
+	addTurnoutStateChangeRequest("TO165", 'N');
+	addTurnoutStateChangeRequest("TO166", 'R');
 
 	executePanelStateChangeRequests();
 }
 
 function CavanaughLeadPath()
 {
-	addTurnoutStateChangeRequest("TO165", 'N');
-	addTurnoutStateChangeRequest("TO166", 'N');
+	addTurnoutStateChangeRequest("TO165", 'R');
+	addTurnoutStateChangeRequest("TO166", 'R');
 
 	executePanelStateChangeRequests();
 }
 
 function MegaWattPath()
 {
-	addTurnoutStateChangeRequest("TO164", 'R');
-	addTurnoutStateChangeRequest("TO166", 'R');
+	addTurnoutStateChangeRequest("TO164", 'N');
+	addTurnoutStateChangeRequest("TO166", 'N');
 
 	executePanelStateChangeRequests();
 }
 
 function GoodMorningCerealPath()
 {
-	addTurnoutStateChangeRequest("TO164", 'N');
-	addTurnoutStateChangeRequest("TO166", 'R');
+	addTurnoutStateChangeRequest("TO164", 'R');
+	addTurnoutStateChangeRequest("TO166", 'N');
 
 	executePanelStateChangeRequests();
 }
