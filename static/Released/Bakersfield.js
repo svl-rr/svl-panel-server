6d936fd596297c1151a1ef6c1bf21f38a9b81211
svlPanelMaker <cerickson@mac>
2013-08-24 00:40:02 -0700

    Numerous Panel Bug Fixes
    Bakersfield polarity and path clean up, panel link errors in
    PanelSchem, PanelSchem3D, Dayton panels

--- static/Released/Bakersfield.js
+++ static/Released/Bakersfield.js
@@ -1,141 +1,144 @@
 function panelInit(evt)
 {
     createPanelTurnout("TO9", false);
-	createPanelTurnout("TO10", true);
+	createPanelTurnout("TO10", false);
 	createPanelTurnout("TO11", false);
-	createPanelTurnout("TO12", false);
+	createPanelTurnout("TO12", true);
 	createPanelTurnout("TO13", false);
 	createPanelTurnout("TO14", false);
 	createPanelTurnout("TO15A", false);
 	createPanelTurnout("TO15B", false);
-	createPanelTurnout("TO16A", true);
-	createPanelTurnout("TO16B", true);
-	createPanelTurnout("TO17A", true);
-	createPanelTurnout("TO17B", true);
+	createPanelTurnout("TO16A", false);
+	createPanelTurnout("TO16B", false);
+	createPanelTurnout("TO17A", false);
+	createPanelTurnout("TO17B", false);
 }
-/*
+
 function track1Path()
 {
-	addTurnoutStateChangeRequest("TO980", 'R');
-	addTurnoutStateChangeRequest("TO982B", 'N');
-    addTurnoutStateChangeRequest("TO984B", 'N');
-    addTurnoutStateChangeRequest("TO986B", 'N');
-    addTurnoutStateChangeRequest("TO990B", 'R');
-
+	addTurnoutStateChangeRequest("TO14", 'R');
+	addTurnoutStateChangeRequest("TO13", 'N');
+    addTurnoutStateChangeRequest("TO12", 'R');
+    addTurnoutStateChangeRequest("TO11", 'N');
+    addTurnoutStateChangeRequest("TO10", 'N');
+    addTurnoutStateChangeRequest("TO9", 'N');
+    addTurnoutStateChangeRequest("TO15B", 'N');
+    addTurnoutStateChangeRequest("TO16B", 'N');
+    addTurnoutStateChangeRequest("TO17B", 'R');
+    
 	executePanelStateChangeRequests();									
 }
 
 function track2Path()
 {
-	addTurnoutStateChangeRequest("TO980", 'R');
-	addTurnoutStateChangeRequest("TO982B", 'N');
-    addTurnoutStateChangeRequest("TO984B", 'N');
-    addTurnoutStateChangeRequest("TO986B", 'N');
-    addTurnoutStateChangeRequest("TO990B", 'N');
+	addTurnoutStateChangeRequest("TO14", 'R');
+	addTurnoutStateChangeRequest("TO13", 'N');
+    addTurnoutStateChangeRequest("TO12", 'R');
+    addTurnoutStateChangeRequest("TO11", 'N');
+    addTurnoutStateChangeRequest("TO10", 'N');
+    addTurnoutStateChangeRequest("TO9", 'N');
+    addTurnoutStateChangeRequest("TO15B", 'N');
+    addTurnoutStateChangeRequest("TO16B", 'R');
 
 	executePanelStateChangeRequests();									
 }
 
 function track3Path()
 {
-	addTurnoutStateChangeRequest("TO980", 'R');
-	addTurnoutStateChangeRequest("TO982B", 'N');
-    addTurnoutStateChangeRequest("TO984B", 'N');
-    addTurnoutStateChangeRequest("TO986B", 'R');
+	addTurnoutStateChangeRequest("TO14", 'R');
+	addTurnoutStateChangeRequest("TO13", 'N');
+    addTurnoutStateChangeRequest("TO12", 'R');
+    addTurnoutStateChangeRequest("TO11", 'N');
+    addTurnoutStateChangeRequest("TO10", 'N');
+    addTurnoutStateChangeRequest("TO9", 'N');
+    addTurnoutStateChangeRequest("TO15B", 'R');
 
 	executePanelStateChangeRequests();									
 }
 
 function track4Path()
 {
-	addTurnoutStateChangeRequest("TO980", 'R');
-	addTurnoutStateChangeRequest("TO982B", 'N');
-    addTurnoutStateChangeRequest("TO984B", 'R');
+	addTurnoutStateChangeRequest("TO14", 'R');
+	addTurnoutStateChangeRequest("TO13", 'N');
+    addTurnoutStateChangeRequest("TO12", 'R');
+    addTurnoutStateChangeRequest("TO11", 'N');
+    addTurnoutStateChangeRequest("TO10", 'N');
+    addTurnoutStateChangeRequest("TO9", 'R');
 
 	executePanelStateChangeRequests();									
 }
 
 function track5Path()
 {
-	addTurnoutStateChangeRequest("TO980", 'R');
-	addTurnoutStateChangeRequest("TO982B", 'R');
+	addTurnoutStateChangeRequest("TO14", 'R');
+	addTurnoutStateChangeRequest("TO13", 'N');
+    addTurnoutStateChangeRequest("TO12", 'R');
+    addTurnoutStateChangeRequest("TO11", 'N');
+    addTurnoutStateChangeRequest("TO10", 'R');
 
 	executePanelStateChangeRequests();									
 }
 
 function track6Path()
 {
-	addTurnoutStateChangeRequest("TO980", 'N');
-	addTurnoutStateChangeRequest("TO982A", 'R');
+	addTurnoutStateChangeRequest("TO14", 'R');
+	addTurnoutStateChangeRequest("TO13", 'N');
+    addTurnoutStateChangeRequest("TO12", 'R');
+    addTurnoutStateChangeRequest("TO11", 'R');
 
 	executePanelStateChangeRequests();									
 }
 
 function track7Path()
 {
-	addTurnoutStateChangeRequest("TO980", 'N');
-	addTurnoutStateChangeRequest("TO982A", 'N');
-	addTurnoutStateChangeRequest("TO984A", 'R');
+	addTurnoutStateChangeRequest("TO14", 'R');
+	addTurnoutStateChangeRequest("TO13", 'N');
+    addTurnoutStateChangeRequest("TO12", 'N');
 
 	executePanelStateChangeRequests();									
 }
 
 function track8Path()
 {
-	addTurnoutStateChangeRequest("TO980", 'N');
-	addTurnoutStateChangeRequest("TO982A", 'N');
-	addTurnoutStateChangeRequest("TO984A", 'N');
-	addTurnoutStateChangeRequest("TO986A", 'R');
+	addTurnoutStateChangeRequest("TO14", 'R');
+	addTurnoutStateChangeRequest("TO13", 'R');
 
 	executePanelStateChangeRequests();									
 }
 
 function track9Path()
 {
-	addTurnoutStateChangeRequest("TO980", 'N');
-	addTurnoutStateChangeRequest("TO982A", 'N');
-	addTurnoutStateChangeRequest("TO984A", 'N');
-	addTurnoutStateChangeRequest("TO986A", 'N');
-	addTurnoutStateChangeRequest("TO990A", 'N');
+	addTurnoutStateChangeRequest("TO14", 'N');
+	addTurnoutStateChangeRequest("TO15A", 'N');
 
 	executePanelStateChangeRequests();									
 }
 
 function track10Path()
 {
-	addTurnoutStateChangeRequest("TO980", 'N');
-	addTurnoutStateChangeRequest("TO982A", 'N');
-	addTurnoutStateChangeRequest("TO984A", 'N');
-	addTurnoutStateChangeRequest("TO986A", 'N');
-	addTurnoutStateChangeRequest("TO990A", 'R');
-	addTurnoutStateChangeRequest("TO989", 'R');
-
+	addTurnoutStateChangeRequest("TO14", 'N');
+	addTurnoutStateChangeRequest("TO15A", 'R');
+    addTurnoutStateChangeRequest("TO16A", 'R');
+    
 	executePanelStateChangeRequests();									
 }
 
 function track11Path()
 {
-	addTurnoutStateChangeRequest("TO980", 'N');
-	addTurnoutStateChangeRequest("TO982A", 'N');
-	addTurnoutStateChangeRequest("TO984A", 'N');
-	addTurnoutStateChangeRequest("TO986A", 'N');
-	addTurnoutStateChangeRequest("TO990A", 'R');
-	addTurnoutStateChangeRequest("TO989", 'N');
-	addTurnoutStateChangeRequest("TO992B", 'R');
-
+	addTurnoutStateChangeRequest("TO14", 'N');
+	addTurnoutStateChangeRequest("TO15A", 'R');
+    addTurnoutStateChangeRequest("TO16A", 'N');
+    addTurnoutStateChangeRequest("TO17A", 'R');
+    
 	executePanelStateChangeRequests();									
 }
 
 function track12Path()
 {
-	addTurnoutStateChangeRequest("TO980", 'N');
-	addTurnoutStateChangeRequest("TO982A", 'N');
-	addTurnoutStateChangeRequest("TO984A", 'N');
-	addTurnoutStateChangeRequest("TO986A", 'N');
-	addTurnoutStateChangeRequest("TO990A", 'R');
-	addTurnoutStateChangeRequest("TO989", 'N');
-	addTurnoutStateChangeRequest("TO992B", 'N');
-	addTurnoutStateChangeRequest("TO993", 'R');
+	addTurnoutStateChangeRequest("TO14", 'N');
+	addTurnoutStateChangeRequest("TO15A", 'R');
+    addTurnoutStateChangeRequest("TO16A", 'N');
+    addTurnoutStateChangeRequest("TO17A", 'N');
 
 	executePanelStateChangeRequests();									
-}*/
+}
