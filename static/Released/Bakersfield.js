6cdf468cc736f5c25c8354bffd97096534917056
svlPanelMaker <cerickson@mac>
2013-08-23 22:06:34 -0700

    Added Basic Bakersfield Panel
    Added Basic Bakersfield Panel

---
+++ static/Released/Bakersfield.js
@@ -0,0 +1,141 @@
+function panelInit(evt)
+{
+    createPanelTurnout("TO9", false);
+	createPanelTurnout("TO10", true);
+	createPanelTurnout("TO11", false);
+	createPanelTurnout("TO12", false);
+	createPanelTurnout("TO13", false);
+	createPanelTurnout("TO14", false);
+	createPanelTurnout("TO15A", false);
+	createPanelTurnout("TO15B", false);
+	createPanelTurnout("TO16A", true);
+	createPanelTurnout("TO16B", true);
+	createPanelTurnout("TO17A", true);
+	createPanelTurnout("TO17B", true);
+}
+/*
+function track1Path()
+{
+	addTurnoutStateChangeRequest("TO980", 'R');
+	addTurnoutStateChangeRequest("TO982B", 'N');
+    addTurnoutStateChangeRequest("TO984B", 'N');
+    addTurnoutStateChangeRequest("TO986B", 'N');
+    addTurnoutStateChangeRequest("TO990B", 'R');
+
+	executePanelStateChangeRequests();									
+}
+
+function track2Path()
+{
+	addTurnoutStateChangeRequest("TO980", 'R');
+	addTurnoutStateChangeRequest("TO982B", 'N');
+    addTurnoutStateChangeRequest("TO984B", 'N');
+    addTurnoutStateChangeRequest("TO986B", 'N');
+    addTurnoutStateChangeRequest("TO990B", 'N');
+
+	executePanelStateChangeRequests();									
+}
+
+function track3Path()
+{
+	addTurnoutStateChangeRequest("TO980", 'R');
+	addTurnoutStateChangeRequest("TO982B", 'N');
+    addTurnoutStateChangeRequest("TO984B", 'N');
+    addTurnoutStateChangeRequest("TO986B", 'R');
+
+	executePanelStateChangeRequests();									
+}
+
+function track4Path()
+{
+	addTurnoutStateChangeRequest("TO980", 'R');
+	addTurnoutStateChangeRequest("TO982B", 'N');
+    addTurnoutStateChangeRequest("TO984B", 'R');
+
+	executePanelStateChangeRequests();									
+}
+
+function track5Path()
+{
+	addTurnoutStateChangeRequest("TO980", 'R');
+	addTurnoutStateChangeRequest("TO982B", 'R');
+
+	executePanelStateChangeRequests();									
+}
+
+function track6Path()
+{
+	addTurnoutStateChangeRequest("TO980", 'N');
+	addTurnoutStateChangeRequest("TO982A", 'R');
+
+	executePanelStateChangeRequests();									
+}
+
+function track7Path()
+{
+	addTurnoutStateChangeRequest("TO980", 'N');
+	addTurnoutStateChangeRequest("TO982A", 'N');
+	addTurnoutStateChangeRequest("TO984A", 'R');
+
+	executePanelStateChangeRequests();									
+}
+
+function track8Path()
+{
+	addTurnoutStateChangeRequest("TO980", 'N');
+	addTurnoutStateChangeRequest("TO982A", 'N');
+	addTurnoutStateChangeRequest("TO984A", 'N');
+	addTurnoutStateChangeRequest("TO986A", 'R');
+
+	executePanelStateChangeRequests();									
+}
+
+function track9Path()
+{
+	addTurnoutStateChangeRequest("TO980", 'N');
+	addTurnoutStateChangeRequest("TO982A", 'N');
+	addTurnoutStateChangeRequest("TO984A", 'N');
+	addTurnoutStateChangeRequest("TO986A", 'N');
+	addTurnoutStateChangeRequest("TO990A", 'N');
+
+	executePanelStateChangeRequests();									
+}
+
+function track10Path()
+{
+	addTurnoutStateChangeRequest("TO980", 'N');
+	addTurnoutStateChangeRequest("TO982A", 'N');
+	addTurnoutStateChangeRequest("TO984A", 'N');
+	addTurnoutStateChangeRequest("TO986A", 'N');
+	addTurnoutStateChangeRequest("TO990A", 'R');
+	addTurnoutStateChangeRequest("TO989", 'R');
+
+	executePanelStateChangeRequests();									
+}
+
+function track11Path()
+{
+	addTurnoutStateChangeRequest("TO980", 'N');
+	addTurnoutStateChangeRequest("TO982A", 'N');
+	addTurnoutStateChangeRequest("TO984A", 'N');
+	addTurnoutStateChangeRequest("TO986A", 'N');
+	addTurnoutStateChangeRequest("TO990A", 'R');
+	addTurnoutStateChangeRequest("TO989", 'N');
+	addTurnoutStateChangeRequest("TO992B", 'R');
+
+	executePanelStateChangeRequests();									
+}
+
+function track12Path()
+{
+	addTurnoutStateChangeRequest("TO980", 'N');
+	addTurnoutStateChangeRequest("TO982A", 'N');
+	addTurnoutStateChangeRequest("TO984A", 'N');
+	addTurnoutStateChangeRequest("TO986A", 'N');
+	addTurnoutStateChangeRequest("TO990A", 'R');
+	addTurnoutStateChangeRequest("TO989", 'N');
+	addTurnoutStateChangeRequest("TO992B", 'N');
+	addTurnoutStateChangeRequest("TO993", 'R');
+
+	executePanelStateChangeRequests();									
+}*/
