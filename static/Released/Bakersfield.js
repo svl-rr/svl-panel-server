d9072c667eed43de853a326877a0a93fc4a04657
svlPanelMaker <cerickson@mac>
2013-09-15 16:45:58 -0700

    Merge branch 'dispatchFriendlyTurnouts' into New-Panels

---
+++ static/Released/Bakersfield.js
@@ -0,0 +1,63 @@
+function panelInit(evt)
+{
+}
+
+function track1Path()
+{
+    executePathArray(["TO14.R", "TO13.N", "TO12.R", "TO11.N", "TO10.N", "TO9.R", "TO15B.N", "TO16B.N", "TO17B.R"]);
+}
+
+function track2Path()
+{
+    executePathArray(["TO14.R", "TO13.N", "TO12.R", "TO11.N", "TO10.N", "TO9.R", "TO15B.N", "TO16B.R"]);
+}
+
+function track3Path()
+{
+    executePathArray(["TO14.R", "TO13.N", "TO12.R", "TO11.N", "TO10.N", "TO9.R", "TO15B.R"]);
+}
+
+function track4Path()
+{
+    executePathArray(["TO14.R", "TO13.N", "TO12.R", "TO11.N", "TO10.N", "TO9.N"]);
+}
+
+function track5Path()
+{
+    executePathArray(["TO14.R", "TO13.N", "TO12.R", "TO11.N", "TO10.R"]);
+}
+
+function track6Path()
+{    
+    executePathArray(["TO14.R", "TO13.N", "TO12.R", "TO11.R"]);
+}
+
+function track7Path()
+{
+    executePathArray(["TO14.R", "TO13.N", "TO12.N"]);
+}
+
+function track8Path()
+{
+    executePathArray(["TO14.R", "TO13.R"]);
+}
+
+function track9Path()
+{
+    executePathArray(["TO14.N", "TO15A.N"]);
+}
+
+function track10Path()
+{    
+    executePathArray(["TO14.N", "TO15A.R", "TO16A.R"]);
+}
+
+function track11Path()
+{
+    executePathArray(["TO14.N", "TO15A.R", "TO16A.N", "TO17A.R"]);
+}
+
+function track12Path()
+{
+    executePathArray(["TO14.N", "TO15A.R", "TO16A.N", "TO17A.N"]);
+}
--- static/Released/Dayton.js
+++ static/Released/Dayton.js
@@ -1,119 +1,88 @@
 function panelInit(evt)
 {
-    // Dodge Mains
-    createPanelTurnout("TO18", true);
-	createPanelTurnout("TO19", false);
-    
-    // Dayton Mains
-    createPanelTurnout("TO125", false);
-	createPanelTurnout("TO126", false);
-	createPanelTurnout("TO127", false);
-    createPanelTurnout("TO128", false);
-	createPanelTurnout("TO129", true);
-    
-    // Dayton Industries
-	createPanelTurnout("TO130A", false);
-    createPanelTurnout("TO130B", false);
-    createPanelTurnout("TO131A", false);
-    createPanelTurnout("TO131B", false);
-    createPanelTurnout("TO132A", false);
-    createPanelTurnout("TO132B", true);
-    createPanelTurnout("TO150", false);
-	createPanelTurnout("TO164", false);
-    createPanelTurnout("TO165", false);
-	createPanelTurnout("TO166", false);
-	createPanelTurnout("TO167", false);
-    createPanelTurnout("TO168", false);
-    createPanelTurnout("TO169", false);
-    createPanelTurnout("TO170", false);
-    createPanelTurnout("TO171", false);
-    createPanelTurnout("TO172", false);
-    createPanelTurnout("TO173", false);
-    createPanelTurnout("TO174", false);
 }
 
 function DPL3Path()
 {
-	addTurnoutStateChangeRequest("TO174", 'N');
-	addTurnoutStateChangeRequest("TO172", 'R');
-
-	executePanelStateChangeRequests();
+	executePathArray(["TO174.R", "TO172.N"]);
 }
 
 function DPL2Path()
 {
-	addTurnoutStateChangeRequest("TO174", 'R');
-	addTurnoutStateChangeRequest("TO172", 'R');
-
-	executePanelStateChangeRequests();
+	executePathArray(["TO174.N", "TO172.N"]);
 }
 
 function DPL1Path()
 {
-	addTurnoutStateChangeRequest("TO173", 'N');
-	addTurnoutStateChangeRequest("TO172", 'N');
-
-	executePanelStateChangeRequests();
+	executePathArray(["TO173.N", "TO172.R"]);
 }
 
 function PeacheyBrothersPath()
 {
-	addTurnoutStateChangeRequest("TO173", 'R');
-	addTurnoutStateChangeRequest("TO172", 'N');
-
-	executePanelStateChangeRequests();
+	executePathArray(["TO173.R", "TO172.R"]);
 }
 
-function WiebeWafersPath()
+function DaytonIndustrialLeadPath()
 {
-	addTurnoutStateChangeRequest("TO167", 'R');
-	//addTurnoutStateChangeRequest("TO169", 'N');
+	executePathArray(["TO170.R", "TO171.R"]);	
+}
 
-	executePanelStateChangeRequests();
+function IndRunaroundNorthPath()
+{
+	executePathArray(["TO170.N", "TO171.R"]);	
+}
+
+function IndRunaroundSouthPath()
+{
+	executePathArray(["TO169.N"]);
+}
+
+function OnthaCRLLeadPath()
+{
+	executePathArray(["TO171.N"]);
 }
 
 function OnthaSkidsPath()
 {
-	addTurnoutStateChangeRequest("TO168", 'R');
+	executePathArray(["TO168.R"]);
+}
 
-	executePanelStateChangeRequests();
+function WiebeOnthaCRLPath()
+{
+       executePathArray(["TO167.N", "TO168.N"]);
+}
+
+function WiebeWafersPath()
+{
+	executePathArray(["TO167.R"]);
+}
+
+function WiebeCRLLeadPath()
+{
+	executePathArray(["TO169.R"]);
 }
 
 function WarehouseNo9Path()
 {
-	addTurnoutStateChangeRequest("TO165", 'R');
-	addTurnoutStateChangeRequest("TO166", 'N');
-
-	executePanelStateChangeRequests();
+	executePathArray(["TO165.N", "TO166.R"]);
 }
 
 function CavanaughLeadPath()
 {
-	addTurnoutStateChangeRequest("TO165", 'N');
-	addTurnoutStateChangeRequest("TO166", 'N');
-
-	executePanelStateChangeRequests();
+	executePathArray(["TO165.R", "TO166.R"]);
 }
 
 function MegaWattPath()
 {
-	addTurnoutStateChangeRequest("TO164", 'R');
-	addTurnoutStateChangeRequest("TO166", 'R');
-
-	executePanelStateChangeRequests();
+	executePathArray(["TO164.N", "TO166.N"]);
 }
 
 function GoodMorningCerealPath()
 {
-	addTurnoutStateChangeRequest("TO164", 'N');
-	addTurnoutStateChangeRequest("TO166", 'R');
-
-	executePanelStateChangeRequests();
+	executePathArray(["TO164.R", "TO166.N"]);
 }
 
 function TeamTrackPath()
 {
-	addTurnoutStateChangeRequest("TO132A", 'N');
-
-	executePanelStateChangeRequests();
+	executePathArray(["TO132A.N"]);
 }
