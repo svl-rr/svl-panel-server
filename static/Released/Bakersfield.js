1dcd0338d33ec121a703457e2a9a94a6c8648358
svlPanelMaker <cerickson@mac>
2013-09-15 16:51:42 -0700

    Corrected Bakersfield.js after conflict
    Corrected Bakersfield.js after conflict

--- static/Released/Bakersfield.js
+++ static/Released/Bakersfield.js
@@ -1,90 +1,25 @@
 function panelInit(evt)
 {
-<<<<<<< HEAD
-    createPanelTurnout("TO9", true);
-	createPanelTurnout("TO10", false);
-	createPanelTurnout("TO11", false);
-	createPanelTurnout("TO12", true);
-	createPanelTurnout("TO13", false);
-	createPanelTurnout("TO14", false);
-	createPanelTurnout("TO15A", false);
-	createPanelTurnout("TO15B", false);
-	createPanelTurnout("TO16A", false);
-	createPanelTurnout("TO16B", false);
-	createPanelTurnout("TO17A", false);
-	createPanelTurnout("TO17B", false);
-=======
->>>>>>> dispatchFriendlyTurnouts
 }
 
 function track1Path()
 {
-<<<<<<< HEAD
-	addTurnoutStateChangeRequest("TO14", 'R');
-	addTurnoutStateChangeRequest("TO13", 'N');
-    addTurnoutStateChangeRequest("TO12", 'R');
-    addTurnoutStateChangeRequest("TO11", 'N');
-    addTurnoutStateChangeRequest("TO10", 'N');
-    addTurnoutStateChangeRequest("TO9", 'R');
-    addTurnoutStateChangeRequest("TO15B", 'N');
-    addTurnoutStateChangeRequest("TO16B", 'N');
-    addTurnoutStateChangeRequest("TO17B", 'R');
-    
-	executePanelStateChangeRequests();									
-=======
     executePathArray(["TO14.R", "TO13.N", "TO12.R", "TO11.N", "TO10.N", "TO9.R", "TO15B.N", "TO16B.N", "TO17B.R"]);
->>>>>>> dispatchFriendlyTurnouts
 }
 
 function track2Path()
 {
-<<<<<<< HEAD
-	addTurnoutStateChangeRequest("TO14", 'R');
-	addTurnoutStateChangeRequest("TO13", 'N');
-    addTurnoutStateChangeRequest("TO12", 'R');
-    addTurnoutStateChangeRequest("TO11", 'N');
-    addTurnoutStateChangeRequest("TO10", 'N');
-    addTurnoutStateChangeRequest("TO9", 'R');
-    addTurnoutStateChangeRequest("TO15B", 'N');
-    addTurnoutStateChangeRequest("TO16B", 'R');
-
-	executePanelStateChangeRequests();									
-=======
     executePathArray(["TO14.R", "TO13.N", "TO12.R", "TO11.N", "TO10.N", "TO9.R", "TO15B.N", "TO16B.R"]);
->>>>>>> dispatchFriendlyTurnouts
 }
 
 function track3Path()
 {
-<<<<<<< HEAD
-	addTurnoutStateChangeRequest("TO14", 'R');
-	addTurnoutStateChangeRequest("TO13", 'N');
-    addTurnoutStateChangeRequest("TO12", 'R');
-    addTurnoutStateChangeRequest("TO11", 'N');
-    addTurnoutStateChangeRequest("TO10", 'N');
-    addTurnoutStateChangeRequest("TO9", 'R');
-    addTurnoutStateChangeRequest("TO15B", 'R');
-
-	executePanelStateChangeRequests();									
-=======
     executePathArray(["TO14.R", "TO13.N", "TO12.R", "TO11.N", "TO10.N", "TO9.R", "TO15B.R"]);
->>>>>>> dispatchFriendlyTurnouts
 }
 
 function track4Path()
 {
-<<<<<<< HEAD
-	addTurnoutStateChangeRequest("TO14", 'R');
-	addTurnoutStateChangeRequest("TO13", 'N');
-    addTurnoutStateChangeRequest("TO12", 'R');
-    addTurnoutStateChangeRequest("TO11", 'N');
-    addTurnoutStateChangeRequest("TO10", 'N');
-    addTurnoutStateChangeRequest("TO9", 'N');
-
-	executePanelStateChangeRequests();									
-=======
     executePathArray(["TO14.R", "TO13.N", "TO12.R", "TO11.N", "TO10.N", "TO9.N"]);
->>>>>>> dispatchFriendlyTurnouts
 }
 
 function track5Path()
