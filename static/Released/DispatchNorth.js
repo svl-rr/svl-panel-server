a209ba54f22e110003b3471f50a24bfe2db1e8a2
svlPanelMaker <cerickson@mac>
2013-10-19 22:33:43 -0700

    Fixed Crossover Authorization for Dispatching Panels
    Fixed Crossover Authorization for Dispatching Panels due to N/R changes
    made to match the actual layout

--- static/Released/DispatchNorth.js
+++ static/Released/DispatchNorth.js
@@ -1,12 +1,22 @@
 function panelInitNorth(evt)
 {
-    var pathArray = ["TO725A.N", "TO725B.N"];
+    var normalCrossoverPathArray = ["TO725A.N", "TO725B.N"];
 
-    for(var i in pathArray)
+    var reverseCrossoverPathArray = [];
+
+    for(var i in normalCrossoverPathArray)
     {
-        var panelTurnout = getPanelTurnoutFromElemID(pathArray[i]);
+        var panelTurnout = getPanelTurnoutFromElemID(normalCrossoverPathArray[i]);
 
         if(panelTurnout != null)
             panelTurnout.allowMultipleNormalAuthorizations = true;
+    }
+    
+    for(var i in reverseCrossoverPathArray)
+    {
+        var panelTurnout = getPanelTurnoutFromElemID(reverseCrossoverPathArray[i]);
+
+        if(panelTurnout != null)
+            panelTurnout.allowMultipleReverseAuthorizations = true;
     }
 }
