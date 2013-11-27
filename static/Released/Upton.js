ed224716587ca9b2b00f52a4f4a7f840beceef0e
svlPanelMaker <cerickson@mac>
2013-11-26 17:26:27 -0800

    Add Upton and Victoria panels
    Add Upton and Victoria panels

---
+++ static/Released/Upton.js
@@ -0,0 +1,83 @@
+function panelInit(evt)
+{
+}
+
+function track1Path()
+{
+	executePathArray(["TO980.R", "TO982B.N", "TO984B.N", "TO986B.N", "TO990B.R"]);
+}
+
+function track2Path()
+{
+	executePathArray(["TO980.R", "TO982B.N", "TO984B.N", "TO986B.N", "TO990B.N"]);
+}
+
+function track3Path()
+{
+	executePathArray(["TO980.R", "TO982B.N", "TO984B.N", "TO986B.R"]);
+}
+
+function track4Path()
+{
+	executePathArray(["TO980.R", "TO982B.N", "TO984B.R"]);
+}
+
+function track5Path()
+{
+	executePathArray(["TO980.R", "TO982B.R"]);
+}
+
+function track6Path()
+{
+	executePathArray(["TO980.N", "TO982A.R"]);
+}
+
+function track7Path()
+{
+	executePathArray(["TO980.N", "TO982A.N", "TO984A.R"]);
+}
+
+function track8Path()
+{
+	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.R"]);
+}
+
+function track9Path()
+{
+	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.N", "TO990A.N"]);
+}
+
+function track10Path()
+{
+	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.N", "TO990A.R", "TO989.R"]);
+}
+
+function track11Path()
+{
+	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.N", "TO990A.R", "TO989.N", "TO992B.R"]);
+}
+
+function track12Path()
+{
+	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.N", "TO990A.R", "TO989.N", "TO992B.N", "TO993.R"]);
+}
+
+function track13Path()
+{
+	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.N", "TO990A.R", "TO989.N", "TO992B.N", "TO993.N"]);
+}
+
+function track14Path()
+{
+	executePathArray(["TO992A.R"]);
+}
+
+function track11LeadPath()
+{
+	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.N", "TO990A.R", "TO989.N"]);
+}
+
+function track12LeadPath()
+{
+	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.N", "TO990A.R", "TO989.N", "TO992B.N"]);
+}
