7077c3bbd971615ca5169676579983acde033627
svlPanelMaker <cerickson@mac>
2013-12-23 20:44:53 -0800

    Path cleanup
    Removed incorrect paths (onclick field) on new panels

--- static/Released/EbbettsEagle_Escape.js
+++ static/Released/EbbettsEagle_Escape.js
@@ -2,57 +2,7 @@ function panelInit(evt)
 {
 }
 
-function AppleseedBoxPath()
-{
-	//executePathArray(["TO.R", "TO.N"]);
-}
-
-function GeneralStorePath()
-{
-	//executePathArray(["TO.R", "TO.N"]);
-}
-
-function WolfgangWaterPath()
-{
-	//executePathArray(["TO.R", "TO.N"]);
-}
-
-function WolfgangWaterLeadPath()
-{
-	//executePathArray(["TO.R", "TO.N"]);
-}
-
-function MidGothamPath()
-{
-	//executePathArray(["TO.R", "TO.N"]);
-}
-
-function GothamBranchPath()
-{
-	//executePathArray(["TO.R", "TO.N"]);
-}
-
-function TimberTransferLeadPath()
-{
-	//executePathArray(["TO.R", "TO.N"]);
-}
-
-function EngineLeadPath()
-{
-	//executePathArray(["TO.R", "TO.N"]);
-}
-
-function CabooseTrackPath()
-{
-	//executePathArray(["TO.R", "TO.N"]);
-}
-
-function EngineTTPath()
-{
-	//executePathArray(["TO.R", "TO.N"]);
-}
-
-function TimberTransferPath()
+function Path()
 {
 	//executePathArray(["TO.R", "TO.N"]);
 }
