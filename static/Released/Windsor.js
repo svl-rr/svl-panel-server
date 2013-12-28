3d34249ad4369808907857a8f78c09797b00c662
svlPanelMaker <cerickson@mac>
2013-12-28 12:56:45 -0800

    Reallocated Windsor addresses into their assigned range

--- static/Released/Windsor.js
+++ static/Released/Windsor.js
@@ -4,80 +4,80 @@ function panelInit(evt)
 
 function track1Path()
 {
-	executePathArray(["TO980.R", "TO982B.N", "TO984B.N", "TO986B.N", "TO990B.R"]);
+	executePathArray(["TO750.R", "TO752B.N", "TO754B.N", "TO756B.N", "TO760B.R"]);
 }
 
 function track2Path()
 {
-	executePathArray(["TO980.R", "TO982B.N", "TO984B.N", "TO986B.N", "TO990B.N"]);
+	executePathArray(["TO750.R", "TO752B.N", "TO754B.N", "TO756B.N", "TO760B.N"]);
 }
 
 function track3Path()
 {
-	executePathArray(["TO980.R", "TO982B.N", "TO984B.N", "TO986B.R"]);
+	executePathArray(["TO750.R", "TO752B.N", "TO754B.N", "TO756B.R"]);
 }
 
 function track4Path()
 {
-	executePathArray(["TO980.R", "TO982B.N", "TO984B.R"]);
+	executePathArray(["TO750.R", "TO752B.N", "TO754B.R"]);
 }
 
 function track5Path()
 {
-	executePathArray(["TO980.R", "TO982B.R"]);
+	executePathArray(["TO750.R", "TO752B.R"]);
 }
 
 function track6Path()
 {
-	executePathArray(["TO980.N", "TO982A.R"]);
+	executePathArray(["TO750.N", "TO752A.R"]);
 }
 
 function track7Path()
 {
-	executePathArray(["TO980.N", "TO982A.N", "TO984A.R"]);
+	executePathArray(["TO750.N", "TO752A.N", "TO754A.R"]);
 }
 
 function track8Path()
 {
-	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.R"]);
+	executePathArray(["TO750.N", "TO752A.N", "TO754A.N", "TO756A.R"]);
 }
 
 function track9Path()
 {
-	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.N", "TO990A.N"]);
+	executePathArray(["TO750.N", "TO752A.N", "TO754A.N", "TO756A.N", "TO760A.N"]);
 }
 
 function track10Path()
 {
-	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.N", "TO990A.R", "TO989.R"]);
+	executePathArray(["TO750.N", "TO752A.N", "TO754A.N", "TO756A.N", "TO760A.R", "TO759.R"]);
 }
 
 function track11Path()
 {
-	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.N", "TO990A.R", "TO989.N", "TO992B.R"]);
+	executePathArray(["TO750.N", "TO752A.N", "TO754A.N", "TO756A.N", "TO760A.R", "TO759.N", "TO762B.R"]);
 }
 
 function track12Path()
 {
-	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.N", "TO990A.R", "TO989.N", "TO992B.N", "TO993.R"]);
+	executePathArray(["TO750.N", "TO752A.N", "TO754A.N", "TO756A.N", "TO760A.R", "TO759.N", "TO762B.N", "TO763.R"]);
 }
 
 function track13Path()
 {
-	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.N", "TO990A.R", "TO989.N", "TO992B.N", "TO993.N"]);
+	executePathArray(["TO750.N", "TO752A.N", "TO754A.N", "TO756A.N", "TO760A.R", "TO759.N", "TO762B.N", "TO763.N"]);
 }
 
 function track14Path()
 {
-	executePathArray(["TO992A.R"]);
+	executePathArray(["TO762A.R"]);
 }
 
 function track11LeadPath()
 {
-	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.N", "TO990A.R", "TO989.N"]);
+	executePathArray(["TO750.N", "TO752A.N", "TO754A.N", "TO756A.N", "TO760A.R", "TO759.N"]);
 }
 
 function track12LeadPath()
 {
-	executePathArray(["TO980.N", "TO982A.N", "TO984A.N", "TO986A.N", "TO990A.R", "TO989.N", "TO992B.N"]);
+	executePathArray(["TO750.N", "TO752A.N", "TO754A.N", "TO756A.N", "TO760A.R", "TO759.N", "TO762B.N"]);
 }
