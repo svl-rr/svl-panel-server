41ff383bd6baeb07e88fc2e2daee592cd65b7cd6
svlPanelMaker <cerickson@mac>
2014-01-11 21:38:02 -0800

    Hallelujah Path Updates

--- static/Released/Hallelujah.js
+++ static/Released/Hallelujah.js
@@ -2,7 +2,47 @@ function panelInit(evt)
 {
 }
 
-function Path()
+function IgoPropane1Path()
 {
-	//executePathArray(["TO.R", "TO.N"]);
+	executePathArray(["TO245.R", "TO252.R"]);
+}
+
+function IgoPropane2Path()
+{
+	executePathArray(["TO245.N", "TO252.R"]);
+}
+
+function MillPath()
+{
+	executePathArray(["TO246.R"]);
+}
+
+function HallelujahIndLeadPath()
+{
+	executePathArray(["TO246.N"]);
+}
+
+function AcmeToolPath()
+{
+	executePathArray(["TO247.R"]);
+}
+
+function DaytonLeadPath()
+{
+	executePathArray(["TO247.N"]);
+}
+
+function ContinuousRunLoopPath()
+{
+	executePathArray(["TO248.R"]);
+}
+
+function Dayton2Path()
+{
+	executePathArray(["TO248.N", "TO249.N"]);
+}
+
+function Dayton1Path()
+{
+	executePathArray(["TO248.N", "TO249.R"]);
 }
