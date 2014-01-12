95e23365fa31e3d4f01e1407472c93234c52d72e
svlPanelMaker <cerickson@mac>
2014-01-11 22:48:56 -0800

    Escape Path Updates

--- static/Released/EbbettsEagle_Escape.js
+++ static/Released/EbbettsEagle_Escape.js
@@ -2,7 +2,37 @@ function panelInit(evt)
 {
 }
 
-function Path()
+function BranchLeadPath()
 {
-	//executePathArray(["TO.R", "TO.N"]);
+	executePathArray(["TO180.N"]);
+}
+
+function RanchSpurPath()
+{
+	executePathArray(["TO180.R"]);
+}
+
+function FaridayFertilizerPath()
+{
+	executePathArray(["TO184B.R", "TO181.R"]);
+}
+
+function CoopFeedMillPath()
+{
+	executePathArray(["TO182.R", "TO184A.R"]);
+}
+
+function EscapeSidingPath()
+{
+	executePathArray(["TO182.R", "TO184A.N", "TO184B.N", "TO181.R"]);
+}
+
+function EscapeBranchRightPath()
+{
+	executePathArray(["TO181.N"]);
+}
+
+function EscapeBranchLeftPath()
+{
+	executePathArray(["TO182.N"]);
 }
