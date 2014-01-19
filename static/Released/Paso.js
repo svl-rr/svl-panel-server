ceb7908063108d0e2d630ece05d369d0c6e64799
svlPanelMaker <cerickson@mac>
2014-01-18 21:27:32 -0800

    Paso Path Updates

--- static/Released/Paso.js
+++ static/Released/Paso.js
@@ -2,8 +2,42 @@ function panelInit(evt)
 {
 }
 
-function track1Path()
+function ScrapYardTrack4Path()
 {
-	//executePathArray(["TO980.R", "TO982B.N", "TO984B.N", "TO986B.N", "TO990B.R"]);
+	executePathArray(["TO568.N", "TO569.N", "TO570.R"]);
 }
 
+function ScrapYardTrack3Path()
+{
+	executePathArray(["TO568.R", "TO569.N", "TO570.R"]);
+}
+
+function ScrapYardTrack2Path()
+{
+	executePathArray(["TO569.R", "TO570.R"]);
+}
+
+function ScrapYardTrack1Path()
+{
+	executePathArray(["TO570.N"]);
+}
+
+function ScrapMillTrack2Path()
+{
+	executePathArray(["TO573.R"]);
+}
+
+function ScrapMillTrack1LeftPath()
+{
+	executePathArray(["TO573.N"]);
+}
+
+function ScrapMillTrack1RightPath()
+{
+	executePathArray(["TO574.N"]);
+}
+
+function ShippingReceivingPath()
+{
+	executePathArray(["TO574.R"]);
+}
