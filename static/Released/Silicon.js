d7b6970aaabf1ba6adf951a0441cdec4fc878c4a
svlPanelMaker <cerickson@mac>
2014-01-21 21:46:00 -0800

    Silicon Path Updates
    Just preliminary on click function assignments

--- static/Released/Silicon.js
+++ static/Released/Silicon.js
@@ -2,8 +2,79 @@ function panelInit(evt)
 {
 }
 
-function track1Path()
+function IdealCementSidingLeftPath()
 {
-	//executePathArray(["TO980.R", "TO982B.N", "TO984B.N", "TO986B.N", "TO990B.R"]);
+    //executePathArray(["TO980.R"]);
 }
 
+function IdealCementSidingRightPath()
+{
+}
+
+function SiliconMainLeftPath()
+{
+}
+
+function SiliconMainMidLeftPath()
+{
+}
+
+function SiliconMainMidRightPath()
+{
+}
+
+function SiliconMainRightPath()
+{
+}
+
+function SiliconSidingLeftPath()
+{
+}
+
+function SiliconSidingMidPath()
+{
+}
+
+function SiliconSidingRightPath()
+{
+}
+
+function IdealCement2LeftPath()
+{
+}
+
+function IdealCement2RightPath()
+{
+}
+
+function IdealCement1LeftPath()
+{
+}
+
+function IdealCement1RightPath()
+{
+}
+
+function FrontRunaroundLeftPath()
+{
+}
+
+function FrontRunaroundRightPath()
+{
+}
+
+function TeamTrackPath()
+{
+}
+
+function QMPath()
+{
+}
+
+function BagHouse2Path()
+{
+}
+
+function BagHouse1Path()
+{
+}
