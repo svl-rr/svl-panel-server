030b0d25361f99b41c44bd1b8e3ad2630aaa605c
svlPanelMaker <cerickson@mac>
2014-01-23 20:30:18 -0800

    Tracy Path Updates
    Preliminary onclick function assignments only

--- static/Released/Tracy.js
+++ static/Released/Tracy.js
@@ -2,8 +2,59 @@ function panelInit(evt)
 {
 }
 
-function track1Path()
+function DairyTrack2Path()
 {
-	//executePathArray(["TO980.R", "TO982B.N", "TO984B.N", "TO986B.N", "TO990B.R"]);
+	//executePathArray(["TO980.R"]);
 }
 
+function DairyTrack1Path()
+{
+}
+
+function SwitcherSpurPath()
+{
+}
+
+function PrivateCarSpurPath()
+{
+}
+
+function Depot3LeftPath()
+{
+}
+
+function Depot2LeftPath()
+{
+}
+
+function Depot1LeftPath()
+{
+}
+
+function Depot3RightPath()
+{
+}
+
+function Depot2RightPath()
+{
+}
+
+function Depot1RightPath()
+{
+}
+
+function EngineServicingPath()
+{
+}
+
+function EngineRunaroundPath()
+{
+}
+
+function EngineReadyPath()
+{
+}
+
+function REASpurPath()
+{
+}
