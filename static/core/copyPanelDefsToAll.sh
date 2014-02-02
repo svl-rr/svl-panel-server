1ed2d3fb9e7a3fc73750c75459518640ed718bea
svlPanelMaker <cerickson@mac>
2014-02-02 00:40:00 -0800

    Complete static directory reorg
    Separates user defined files from core application files

---
+++ static/core/copyPanelDefsToAll.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+inputLib="PanelDefs.svg"
+
+if [ -f "$inputLib" ]
+then
+	echo "$inputLib exists! Finding definitions section..."
+	awk '/<defs/,/<\/defs>/' "$inputLib" > /tmp/PanelDefs.tmp
+	
+	FILES=../user/*.svg
+	for f in $FILES
+	do
+		if [[ "$f" != "$inputLib" ]] && [[ "$f" != "../user/index.svg" ]] && [[ "$f" != "../user/indexAlt.svg" ]];
+		then
+			echo "Processing $f file..."
+						
+			cp "$f" "$f.orig"
+			awk '(NR==1),/<defs/' "$f" > /tmp/f1.tmp
+			awk '/<\/defs>/,NevERMatcHthiS' "$f" > /tmp/f2.tmp
+			
+			awk '$0!~/<defs/' /tmp/f1.tmp > /tmp/f3.tmp 
+			awk '(NR==2),NevERMatcHthiS' /tmp/f2.tmp > /tmp/f4.tmp
+			
+			cat /tmp/f3.tmp /tmp/PanelDefs.tmp /tmp/f4.tmp > "$f"
+			
+			rm /tmp/f1.tmp /tmp/f2.tmp /tmp/f3.tmp /tmp/f4.tmp
+		fi
+	done
+	
+	rm /tmp/PanelDefs.tmp
+fi
+
---
+++ static/core/turnoutAddressUsage.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+cd ../user
+grep TO *.svg > turnoutAddressList.tmp
+#remove closing xml tags
+perl -p -i -e 's/ \/>//g' turnoutAddressList.tmp
+#remove individual route segment identifiers
+perl -p -i -e 's/\.R//g' turnoutAddressList.tmp
+perl -p -i -e 's/\.N//g' turnoutAddressList.tmp
+#remove id
+perl -p -i -e 's/id=//g' turnoutAddressList.tmp
+#remove quotes
+perl -p -i -e 's/"//g' turnoutAddressList.tmp
+#remove colon
+perl -p -i -e 's/://g' turnoutAddressList.tmp
+#remove file extenstion
+perl -p -i -e 's/\.svg//g' turnoutAddressList.tmp
+#remove whitespace
+perl -p -i -e 's/ +/ /g' turnoutAddressList.tmp
+#remove index, indexAlt, PanelDefs.svg entries
+perl -p -i -e 's/^index\s.*$//g' turnoutAddressList.tmp
+perl -p -i -e 's/^indexAlt\s.*$//g' turnoutAddressList.tmp
+perl -p -i -e 's/PanelDefs\sTO\d+[A-Z]?//g' turnoutAddressList.tmp
+grep -v "^$" turnoutAddressList.tmp > turnoutAddressList3.tmp
+mv turnoutAddressList3.tmp turnoutAddressList.tmp
+#sort results
+sort -d turnoutAddressList.tmp > turnoutAddressList2.tmp
+mv turnoutAddressList2.tmp turnoutAddressList.tmp
+#remove duplicates
+perl -ne 'print unless $seen{$_}++' turnoutAddressList.tmp > turnoutAddressBlockRoutes.txt
+rm turnoutAddressList.tmp
+#find unique addresses
+cp turnoutAddressBlockRoutes.txt turnoutAddressUnique.tmp
+perl -p -i -e 's/[A-Z]$//g' turnoutAddressUnique.tmp
+#remove duplicates
+perl -ne 'print unless $seen{$_}++' turnoutAddressUnique.tmp > turnoutAddressBlockUnique.txt
+rm turnoutAddressUnique.tmp
+#find layout unique
+cp turnoutAddressBlockUnique.txt turnoutAddressLayoutUnique.txt
+perl -p -i -e 's/^\w+\sTO//g' turnoutAddressLayoutUnique.txt
+sort -g turnoutAddressLayoutUnique.txt > turnoutAddressLayoutUnique.tmp
+perl -ne 'print unless $seen{$_}++' turnoutAddressLayoutUnique.tmp > turnoutAddressLayoutUnique.txt
+rm turnoutAddressLayoutUnique.tmp
+#more turnoutAddressBlockRoutes.txt
+#more turnoutAddressBlockUnique.txt
+#more turnoutAddressLayoutUnique.txt
+wc -l turnoutAddress*.txt
+cd ../core
