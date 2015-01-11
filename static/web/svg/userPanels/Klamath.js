72624f900ad911cff4af6f05d0d92fc46c99b66b
svlPanelMaker <cerickson@mac>
2015-01-11 13:54:42 -0800

    Merge branch 'ws-proxy'

--- static/web/svg/userPanels/Klamath.js
+++ static/web/svg/userPanels/Klamath.js
@@ -2,7 +2,67 @@ function panelInit(evt)
 {
 }
 
-function Path()
+function Track1Path()
 {
-	//executePathArray(["TO.R", "TO.N"]);
+	executePathArray(["TO393.R", "TO394.N", "TO396.R", "TO399.N", "TO375.R"]);
+}
+
+function Track2Path()
+{
+	executePathArray(["TO393.N", "TO394.N", "TO396.R", "TO399.N", "TO375.R"]);
+}
+
+function Track3Path()
+{
+	executePathArray(["TO394.R", "TO396.R", "TO399.N", "TO375.R"]);
+}
+
+function Track4Path()
+{
+	executePathArray(["TO395.N", "TO396.N", "TO399.N", "TO375.R"]);
+}
+
+function Track5Path()
+{
+	executePathArray(["TO395.R", "TO396.N", "TO399.N", "TO375.R"]);
+}
+
+function Track6Path()
+{
+	executePathArray(["TO398.R", "TO399.R", "TO375.R"]);
+}
+
+function Track7Path()
+{
+	executePathArray(["TO397.R", "TO398.N", "TO399.R", "TO375.R"]);
+}
+
+function Track8Path()
+{
+	executePathArray(["TO397.N", "TO398.N", "TO399.R", "TO375.R"]);
+}
+
+function KlamathMainJasperPath()
+{
+	executePathArray(["TO377.N"]);
+}
+
+function KlamathSidingJasperPath()
+{
+	executePathArray(["TO377.R"]);
+}
+
+function KlamathMainKalamazooPath()
+{
+	executePathArray(["TO376.N", "TO375.N"]);
+}
+
+function KlamathSidingKalamazooPath()
+{
+	executePathArray(["TO376.R", "TO375.N"]);
+}
+
+function JasperKlamathPath()
+{
+	executePathArray(["TO330.R"]);
 }
