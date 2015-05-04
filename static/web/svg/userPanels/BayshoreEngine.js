c7119d78166c27285d9f4e2531a058d750547261
svlPanelMaker <cerickson@mac>
2015-05-03 22:40:47 -0700

    SPROG Turntable Support
    Initial support for controlling turntable using SPROG

--- static/web/svg/userPanels/BayshoreEngine.js
+++ static/web/svg/userPanels/BayshoreEngine.js
@@ -1,9 +1,43 @@
+var JMRI_TURNTABLE_OBJID_PREFIX = "ST";
+
 function panelInit(evt)
 {
     //setLEDColorByID('dispatchMainlineLockedLED', mainlineLocked == true ? "#ff0000" : "off");
 }
 
-function arrivalLeadPath()
+function addTurntableStateChangeRequest(id)
 {
+    var turnoutAddr = getDCCAddr(id);
+    
+	stateChangeRequests.push(new ServerObject(JMRI_TURNTABLE_OBJID_PREFIX + turnoutAddr, SERVER_TYPE_TURNOUT, 'N'));
+}
+
+function setTurntableState(id)
+{
+    var savedStateChangeRequests = stateChangeRequests;
+
+    stateChangeRequests = [];
+    
+    addTurntableStateChangeRequest(id);
+    
+    executePanelStateChangeRequests();
+    
+    stateChangeRequests = savedStateChangeRequests;
+}
+
+function setTurntableTrack(num)
+{
+    setTurntableState('ST' + (num + 225));
+}
+
+ttLead1Path()
+{
+    setTurntableTrack(1);
+    //executePathArray(["TO36.N", "TO38.R", "TO23.R"]);
+}
+
+ttLead2Path()
+{
+    setTurntableTrack(2);
     //executePathArray(["TO36.N", "TO38.R", "TO23.R"]);
 }
