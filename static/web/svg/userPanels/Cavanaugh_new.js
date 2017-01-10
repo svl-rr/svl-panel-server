9bb1b34038e38821f434369f11c7d3cc70caa4a2
svlPanelMaker <cerickson@mac>
2017-01-10 12:35:21 -0800

    Added prelim new Cavanaugh panel/Update Assignment Docs
    -Added prelim new Cavanaugh panel
    -Updated turnout assignment documentation

---
+++ static/web/svg/userPanels/Cavanaugh_new.js
@@ -0,0 +1,58 @@
+function panelInitPreSocket(evt)
+{
+}
+
+function buzzBeer2Path()
+{
+	executePathArray(["TO104.N", "TO105.R"]);
+}
+
+function buzzBeer1Path()
+{
+	executePathArray(["TO104.R", "TO105.R"]);
+}
+
+function buzzBeerLeadPath()
+{
+	executePathArray(["TO105.R"]);
+}
+
+function slipperShipperPath()
+{
+	executePathArray(["TO103.R"]);
+}
+
+function cavanaughJunkPath()
+{
+	executePathArray(["TO102.R"]);
+}
+
+function departureMain1Path()
+{
+	executePathArray(["TO105.N", "TO100A.N"]);
+}
+
+function departureMain2Path()
+{
+	executePathArray(["TO103.N"]);
+}
+
+function departureMain3Path()
+{
+	executePathArray(["TO106.N", "TO101.N"]);
+}
+
+function daytonIndustrialPath()
+{
+	executePathArray(["TO106.R"]);
+}
+
+function arrivalMain1Path()
+{
+	executePathArray(["TO101.R"]);	
+}
+
+function arrivalMain2Path()
+{
+	executePathArray(["TO102.N", "TO100B.N"]);	
+}
