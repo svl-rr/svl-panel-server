7e7a125c52be16ec9148d1799bb48ebc48bdc529
svlPanelMaker <cerickson@me.com>
2021-03-15 00:18:04 -0700

    Update ServerComms.js
    Added method (get, post) info to JSON messages sent to JMRI server. This is necessary for recent versions of JMRI (~4.15 or later?). Minor debug logging improvements.  This commit should properly preserve userName options.

--- static/web/svg/core/ServerComms.js
+++ static/web/svg/core/ServerComms.js
@@ -56,7 +56,7 @@ function initJMRISocketInstance()
     
     jmriSocket.onopen = function ()
     {
-        console.log("connected!");
+        console.log("connected via JMRI Socket!");
 
         // Set up ping to maintain connection
         setInterval(function() { jmriSocket.send('{"type":"ping"}'); }, 10000);
@@ -80,7 +80,7 @@ function initJMRISocketInstance()
 
     jmriSocket.onclose = function (e)
     {
-        console.log("disconnected "+ e.code);
+        console.log("disconnected from JMRI socket "+ e.code);
         
         socketStatus = SOCKET_DISCONNECTED;
         
@@ -94,12 +94,14 @@ function initNodeSocketInstance()
 
     nodeSocket.on('connect', function ()
     {
+        console.log("connected via Node socket");
+
         socketStatus = SOCKET_CONNECTED;
         handleSocketConnect();
     });
 
     // on an response from the server, server will send array of updated elements
-    // not all elements will necesarily be on this panel
+    // not all elements will necessarily be on this panel
     nodeSocket.on('update', function(data)
     {
         var msgObj = JSON.parse(data);
@@ -122,6 +124,8 @@ function initNodeSocketInstance()
     
     nodeSocket.on('disconnect', function ()
     {
+        console.log("disconnected from Node socket");
+
         socketStatus = SOCKET_DISCONNECTED;        
         handleSocketDisconnect();
     });
@@ -331,11 +335,11 @@ function getJMRIObjects()
 function getJMRIObjectsOfType(type)
 {
     if(type == SERVER_TYPE_DISPATCH)
-        serverSendRawJSON('{"type":"list","list":"memories"}', SERVER_GET);
+        serverSendRawJSON('{"type":"list","list":"memories", "method":"get"}', SERVER_GET);
     else if(type == SERVER_TYPE_SENSOR)
-        serverSendRawJSON('{"type":"list","list":"sensors"}', SERVER_GET);
+        serverSendRawJSON('{"type":"list","list":"sensors", "method":"get"}', SERVER_GET);
     else if(type == SERVER_TYPE_TURNOUT)
-        serverSendRawJSON('{"type":"list","list":"turnouts"}', SERVER_GET);
+        serverSendRawJSON('{"type":"list","list":"turnouts", "method":"get"}', SERVER_GET);
     else
         alert("Unnsupported type (" + type + ") passed to getAllJMRIObjects()");
 }
@@ -415,14 +419,14 @@ function getAsSetJSONString(serverObj)
         else
             jmriState = undefinedStateMap.jmri;
     
-        return '{"type":"turnout","data":{"name":"' + serverObj.name + '","state":"' + jmriState + '"}}';
+        return '{"type":"turnout","data":{"name":"' + serverObj.name + '","state":"' + jmriState + '"}, "method":"post"}';
     }
     else if (serverObj.type == SERVER_TYPE_SENSOR) {
         var nameField = serverObj.useUserName ? "userName" : "name";
-        return '{"type":"sensor","data":{"' + nameField + '":"' + serverObj.name + '","state":"' + serverObj.value + '"}}';
+        return '{"type":"sensor","data":{"' + nameField + '":"' + serverObj.name + '","state":"' + serverObj.value + '"}, "method":"post"}';
     }
     else if(serverObj.type == SERVER_TYPE_DISPATCH)
-        return '{"type":"memory","data":{"name":"IM' + serverObj.name + '","value":"' + serverObj.value + '"}}';
+        return '{"type":"memory","data":{"name":"IM' + serverObj.name + '","value":"' + serverObj.value + '"}, "method":"post"}';
     
     return null;
 }
@@ -430,11 +434,11 @@ function getAsSetJSONString(serverObj)
 function getAsGetJSONString(serverObj)
 {
     if(serverObj.type == SERVER_TYPE_TURNOUT)
-        return '{"type":"turnout","data":{"name":"' + serverObj.name + '"}}';
+        return '{"type":"turnout","data":{"name":"' + serverObj.name + '"}, "method":"get"}';
     else if(serverObj.type == SERVER_TYPE_SENSOR)
-        return '{"type":"sensor","data":{"name":"' + serverObj.name + '"}}';
+        return '{"type":"sensor","data":{"name":"' + serverObj.name + '"}, "method":"get"}';
     else if(serverObj.type == SERVER_TYPE_DISPATCH)
-        return '{"type":"memory","data":{"name":"IM' + serverObj.name + '"}}';
+        return '{"type":"memory","data":{"name":"IM' + serverObj.name + '"}, "method":"get"}';
     
     return null;
 }
